1f6e6ac4a2446db84fa0e88e01fea3b5f655905f
Bas van de Wiel <bas@maxserv.com>
2016-12-02 15:37:40 +0100

    [BUGFIX] Stop respecting robots.txt and clean up your mess after spidering.

--- spider.sh
+++ spider.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget -m $1 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt
+wget -e robots=off -m $1 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt
+rm -rf ./$1
+
