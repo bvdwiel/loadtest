d9782943b48217f1d25f99f9bc1454cee3f42ab8
Bas van de Wiel <bas@maxserv.com>
2016-12-05 12:28:30 +0100

    [FEATURE] Check for usable SQLite command and cleanup working dir.

--- parseresults.sh
+++ parseresults.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-#$results = ls | grep result\.txt
+SQLITECMD=`which sqlite3`
+if [ ! -x $SQLITECMD ]
+then
+  echo "Cannot use sqlite3 command. Please check if SQLite3 is installed on this system and you have permission to use it."
+  exit 1
+fi
+if [ -f results.db ]
+then
+  rm results.db
+fi
+
 for result in c*result.txt
 do
  cat $result | grep -e '^HTTP/1.1,4' > $result._errors.txt
