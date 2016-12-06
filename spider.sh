60a971b3506469c27134eaed046f55e38869699e
Bas van de Wiel <bas@maxserv.com>
2016-12-06 08:37:27 +0100

    [BUGFIX] Catch missing awk in spider.sh

--- spider.sh
+++ spider.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 WGETCMD=`which wget`
+AWKCMD=`which awk`
 if [ ! -x $WGETCMD ] 
 then
   echo "Can't use the wget command. Make sure wget is installed and you have permission to use it."
+  exit 1
+fi
+if [ ! -x $AWKCMD ]
+then
+  echo "Can't use the awk command. Make sure awk is installed and you have permission to use it."
   exit 1
 fi
 if [ -f urls.txt ] 
