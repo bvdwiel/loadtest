bc828312fb6296b3a86c47a517200805e0105c58
Bas van de Wiel <bas@maxserv.com>
2016-12-06 08:39:52 +0100

    [FEATURE] Hardcode dotest.sh to do my workplace default stress test for now.

--- dotest.sh
+++ dotest.sh
@@ -6,4 +6,10 @@ then
   echo "Unable to use the siege command. Make sure Siege is installed and you have permission to use it."
   exit 1
 fi
-$SIEGECMD -f urls.txt -c10 -i -t1M -l > c10.txt
+$SIEGECMD -f urls.txt -c10 -i -t10M -l > c10.txt
+$SIEGECMD -f urls.txt -c20 -i -t10M -l > c20.txt
+$SIEGECMD -f urls.txt -c30 -i -t10M -l > c30.txt
+$SIEGECMD -f urls.txt -c40 -i -t10M -l > c40.txt
+$SIEGECMD -f urls.txt -c50 -i -t10M -l > c50.txt
+$SIEGECMD -f urls.txt -c60 -i -t10M -l > c60.txt
+
