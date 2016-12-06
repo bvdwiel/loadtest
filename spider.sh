a2b89fdf457eefbdd4e9b8487e645e06b9a2abc4
Bas van de Wiel <bas@maxserv.com>
2016-12-06 08:49:50 +0100

    [BUGFIX] Make spider.sh ignore invalid SSL certificates.

--- spider.sh
+++ spider.sh
@@ -15,6 +15,6 @@ if [ -f urls.txt ]
 then
   rm urls.txt
 fi
-$WGETCMD -e robots=off -m $1 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt
+$WGETCMD -e robots=off --no-check-certificate -m $1 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt
 rm -rf ./$1
 
